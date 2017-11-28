e1386bc3071983bb88a31d1c7e6ea95fd1c8e5d7
lts010 <34074926+lts010@users.noreply.github.com>
2017-11-28 15:46:53 -0500

    Create visualizeText.py

---
+++ visualizeText.py
@@ -0,0 +1,10 @@
+"""
+   Visualize data using Matplotlib.
+   CSCI 203 project
+   Spring 2017
+   Student name(s): Logan Stiles and Alex Rabinovich
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+main()
