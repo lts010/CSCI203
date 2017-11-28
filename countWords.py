2d9cfed31b125575580178b85fa418b05ec6166c
lts010 <34074926+lts010@users.noreply.github.com>
2017-11-28 15:43:11 -0500

    Update countWords.py

--- countWords.py
+++ countWords.py
@@ -1,1 +1,9 @@
+"""
+   Count word frequency
+   CSCI 203 project
+   Fall 2017
+   Student names(s): Logan Stiles and Alex Rabinovich
+"""
+from collections import OrderedDict       # ordered dictionary for printing
 
+main()
