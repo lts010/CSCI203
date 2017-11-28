41904bdd5b20ca511459ea5f7d9de5e7c40939ce
lts010 <34074926+lts010@users.noreply.github.com>
2017-11-28 16:28:54 -0500

    Update countWords.py

--- countWords.py
+++ countWords.py
@@ -9,11 +9,24 @@ from collections import OrderedDict       # ordered dictionary for printing
 #main()
 
 def textPrep(filename):
-   f = open(fileName,'rt', encoding = 'UTF-8')
-   textString = f.read()
-   textList = textString.split()
+    f = open(fileName,'rt', encoding = 'UTF-8')
+    textString = f.read()
+    textList = textString.split()
    
-   for i in range(len(textList)):
-      textList[i] = textList[i].strip('-,.:;!?()[]"')
-      textList[i] = textList[i].lower()
-   
+    for i in range(len(textList)):
+       textList[i] = textList[i].strip('-,.:;!?()[]"')
+       textList[i] = textList[i].lower()
+
+def wordCount(textList):       
+    frequencyDict = {}
+    stopF = open('stopwords.txt')
+    stopText = stopF.read()
+    stopList = stopText.split()
+    
+    for w in textList:
+        if w in stopList:
+            continue
+        if w not in d:
+            frequencyDict[w] = 1
+        else:
+            frequencyDict[w] += 1
