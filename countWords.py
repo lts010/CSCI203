8167be3b21a2335d2938fa05fb2aa6608c7bfd70
arabinov4 <34074940+arabinov4@users.noreply.github.com>
2017-12-03 19:20:49 -0500

    Update countWords.py

--- countWords.py
+++ countWords.py
@@ -116,3 +116,56 @@ main()
 
 import doctest
 doctest.testmod()
+
+import collections
+
+
+###################### This is my version of the code - take the lines that are relevant
+
+def textPrep(filename):
+   f = open(filename,'rt', encoding = 'UTF-8')
+   textString = f.read()
+   textList = textString.split()
+   
+   for i in range(len(textList)):
+      textList[i] = textList[i].strip('-,.:;!?()[]"')
+      textList[i] = textList[i].lower()
+   return textList
+
+def wordCount(textList):       
+   
+   wordCounter = {}
+   stopF = open('stopwords.txt')
+   stopText = stopF.read()
+   stopList = stopText.split()
+
+   
+    
+   for w in textList:
+      if w in stopList:
+         continue
+      if w not in wordCounter:
+         wordCounter[w] = 1
+      else:
+         wordCounter[w] += 1
+   sortedDict = collections.OrderedDict(sorted(wordCounter.items(), key=lambda t: t[1], reverse = True))
+   print('Unique words count:', len(sortedDict))
+   keysList = list(sortedDict.keys())
+   valuesList = list(sortedDict.values())
+   n = len(sortedDict)
+   print('Top 20 Values')
+   for i in range(0,20):
+      print(str(i+1) + ". " + keysList[i],':',valuesList[i])
+
+
+def dealWithHyphens(aList):
+   ''' Removes double hyphens '''
+   n = len(aList)
+   for i in range(n):
+      if '--' in aList[i]:
+         h_index = aList[i].find('--')
+         newWord = aList[i][h_index + 2:]
+         aList += [newWord]
+         aList[i] = aList[i][0:h_index]
+   return aList
+
